471c68bceec4ca76c22a7868aed7d50dcb8e76f8
edaw003 <flore_dan@yahoo.com>
2020-02-10 20:56:56 +0200

    creating arp packets

--- netScan.py
+++ netScan.py
@@ -1,9 +1,20 @@
 import scapy.all as scapy
 
 
-deg findMac(ip):
-    arpReq = scapy.ARP(pdest=ip)
-    arpBroad = scapy.Ether(dest="ff:ff:ff:ff:ff:ff")
+def findMac(ip):
+    arpReq = scapy.ARP(pdst=ip) # create ARP request
+    
+    #print(arpReq.summary())
+    #scapy.ls(scapy.ARP())
+
+    arpBroad = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") # create Ethernet object
+    #print(arpBroad.summary())
+    #scapy.ls(scapy.Ether())
 
     arpReqBroad = arpBroad/arpReq
-    
+
+    #arpReqBroad.show()
+    answ, unasnw = scapy.srp(arpReqBroad, timeout=1)
+    print(answ.summary())
+
+findMac("10.0.2.1/24")
